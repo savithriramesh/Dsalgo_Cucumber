104cd89b7b0153884a3db110ebaa68e08c7f5da7
savithriramesh <savithriramesh11@gmail.com>
2022-10-16 20:46:26 -0500

    modified

--- src/test/java/myTestRunner/TestRunner.java
+++ src/test/java/myTestRunner/TestRunner.java
@@ -4,15 +4,33 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		
-		plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}, //reporting purpose
-		monochrome=true,  //console output
-		//tags = "@array or @array_pyn", //tags from feature file
-		features = {"C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Array_Dsalgo.feature", "C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Queue_Dsalgo.feature"}, //location of feature files
-		glue= "stepDefinition") //location of step definition files
+
+		plugin = { "pretty", "html:target/Dsalgo.html", "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}, // reporting purpose
+		monochrome = true, // console output
+		 //tags = "@array or @array_pyn", //tags from feature file
+		features = { "C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Stack.feature",
+				"C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Queue_Dsalgo.feature",
+				"C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\DataStructure_Dsalso.feature",
+				"C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Z_Login_Dsalgo.feature",
+				"C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Array_Dsalgo.feature",
+				"C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Graph.feature"}, // location of
+		// feature files
+		glue = "stepDefinition") // location of step definition files
 
 public class TestRunner extends AbstractTestNGCucumberTests {
-		
 
 }
 //html:target/Dsalgo.html
+
+//C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Array_Dsalgo.feature", "C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Queue_Dsalgo.feature
+
+/*
+ * "C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Graph.feature",
+ * "C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Stack.feature",
+ * "C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\DataStructure_Dsalso.feature",
+ * "C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Login_Dsalgo.feature",
+ * "C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Array_Dsalgo.feature",
+ * "C:\\Workspace\\first-project\\Ds-algo_Cucumber\\Features\\Queue_Dsalgo.feature"
+ */
+
+//io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm
